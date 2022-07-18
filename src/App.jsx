a0d21801ac3fe26083a6bdbb487285e0637be332
JamKyu <jamkyu@pm.me>
2022-07-18 15:50:56 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/search" element={<SearchPage />}>
+          </Route>
+          <Route path="/" element={<Home />}>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
